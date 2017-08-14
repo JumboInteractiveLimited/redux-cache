8218ccfe1095f32c52288a2542bb19ec8c0e15e5
patrick <pgord1@gmail.com>
2017-08-14 19:29:16 +1000

    Added coverage to updateState fn

--- src/__tests__/cacheEnhancer.test.ts
+++ src/__tests__/cacheEnhancer.test.ts
@@ -1,11 +1,28 @@
 var MockDate = require("mockdate");
 
-import cacheEnhancer, { liftReducer } from "../cacheEnhancer";
+import cacheEnhancer, { liftReducer, updateState } from "../cacheEnhancer";
 import { INVALIDATE_CACHE } from '../actions';
 import { DEFAULT_KEY } from "../constants";
 
+let reducer;
+let state;
+let action;
+
 beforeEach(() => {
 	MockDate.set(0);
+	reducer = jest.fn();
+	state = {
+		myReducer: {
+			[DEFAULT_KEY]: 2000,
+			myValue: "someValue"
+		}
+	}
+	action = {
+		type: INVALIDATE_CACHE,
+		payload: {
+			reducers: ["myReducer"]
+		}
+	}
 });
 
 describe('cacheEnhancer', () => {
@@ -13,26 +30,6 @@ describe('cacheEnhancer', () => {
 });
 
 describe('liftReducer', () => {
-	let reducer;
-	let state;
-	let action;
-
-	beforeEach(() => {
-		reducer = jest.fn();
-		state = {
-			myReducer: {
-				[DEFAULT_KEY]: 2000,
-				myValue: "someValue"
-			}
-		}
-		action = {
-			type: INVALIDATE_CACHE,
-			payload: {
-				reducers: ["myReducer"]
-			}
-		}
-	});
-
 	describe("unknown action", () => {
 		it("should call the reducer with the given state and action", () => {
 			action = {
@@ -60,4 +57,36 @@ describe('liftReducer', () => {
 			expect(output).toEqual(expectedOutput);
 		})
 	})
+});
+
+
+describe('updateState', () => {
+	it("should update the state for cache enabled reducers that match", () => {
+		const newState = updateState(["myReducer"], state, {});
+		expect(state.myReducer[DEFAULT_KEY]).toBe(null);
+	});
+
+	it("should not update reducers that match but are not cache enabled", () => {
+		const modifiedState = {
+			...state,
+			notCacheEnabled: {
+				someKey: "someValue"
+			}
+		}
+
+		const newState = updateState(["notCacheEnabled"], modifiedState, {})
+		expect(newState.notCacheEnabled).not.toHaveProperty(DEFAULT_KEY);
+	});
+
+	it("should ignore reducers that do not match", () => {
+		const modifiedState = {
+			...state,
+			notCacheEnabled: {
+				someKey: "someValue"
+			}
+		}
+
+		const newState = updateState(["myReducer", "doesntMatch"], modifiedState, {})
+		expect(newState).not.toHaveProperty("doesntMatch");
+	});
 });
--- src/cacheEnhancer.ts
+++ src/cacheEnhancer.ts
@@ -19,14 +19,26 @@ const logResult = (name: string, array: string[]): void => {
 	console.log("redux-cache: %s: %s", name, array.join(", ") || "none found");
 };
 
-export const updateState = (reducersToInvalidate, currentState, { log = false }) => {
+/**
+ * // TODO: Add info here
+ * 
+ * @param {string[]} reducersToInvalidate - List of reducers to invalidate
+ * @param {object} currentState - The current and already reduced state.
+ * @param {object} [config={}] - Configuration options
+ * @param {boolean} [config.log=false] - Whether or not to output log information. Useful for debugging.
+ */
+export const updateState = (reducersToInvalidate, currentState, config) => {
+	const { log = false } = config;
 	const newState = { ...currentState };
 	const stateKeys = Object.keys(newState);
+
+	// TODO: We should really return early from this fn if matchedReducers or cacheEnabledReducers
+	// is empty. Otherwise we're just reducing/filtering on empty arrays for no reason.
 
 	// We filter to those reducers which exist in the application state tree
 	const matchedReducers = reducersToInvalidate.filter(reducerKey => {
 		const matched = (stateKeys.indexOf(reducerKey) !== -1);
-		if (!matched) { console.log("redux-cache: did not match %s reducer to state tree", reducerKey); }
+		if (!matched && log) { console.log("redux-cache: did not match %s reducer to state tree", reducerKey); }
 		return matched;
 	});
 	if (log) { logResult("matchedReducers", matchedReducers); }
@@ -64,6 +76,7 @@ export const liftReducer = (reducer, config) => (state, action) => {
 }
 
 /**
+ * // TODO: add info here
  * 
  * @param config 
  * @returns {Object} - returns the enhanced store
